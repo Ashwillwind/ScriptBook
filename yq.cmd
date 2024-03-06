c438f472049cc913fc864eb6145d4a59577ceeb6
Ashwillwind <162058357+Ashwillwind@users.noreply.github.com>
2024-03-06 23:01:14 +0300

    more

--- yq.cmd
+++ yq.cmd
@@ -1,7 +1,7 @@
 ::@ECHO OFF
 @SET YL=%1
 @IF [%1]==[-h] GOTO :HELP
-yt-dlp -F -f best https://www.youtube.com/watch?v=%YL% | find  & exit
+yt-dlp -F -f best https://www.youtube.com/watch?v=%YL% | more  & exit
 
 @GOTO :EOF
 :HELP
