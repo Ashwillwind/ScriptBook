e7cb8b58b37f86c12d41a3d6b1282421aa6fb10b
Ashwillwind <162058357+Ashwillwind@users.noreply.github.com>
2024-03-06 23:02:54 +0300

    [`] skip parametr!

--- ya.cmd
+++ ya.cmd
@@ -1,23 +1,48 @@
-::@ECHO OFF
+@ECHO OFF
 @SET YL=%1
-@SET YT=bestaudio
+@SET YT=-f bestaudio
 @SET VL=75
+@SET FA=
+@SET TP=1.0
 
-@IF NOT [%2]==[] SET YT=%2
-@IF NOT [%3]==[] (IF NOT [%3]==[ogg] SET VL=%3)
-@IF [%3]==[ogg] SET FT=-f ogg
-@IF [%3]==[q] SET FT= & SET VL=75
-@IF NOT [%4]==[] SET ST=-ss %4
+@IF NOT [%2]==[] SET VL=%2
+@IF [%2]==[`] SET VL=75
+
+@IF NOT [%3]==[] SET YT=-f %3
+@IF [%3]==[`] SET YT=-f bestaudio
+
+@IF [%4]==[ogg] SET FA=-f ogg 
+@IF [%4]==[`] SET FA=
+
+@IF NOT [%5]==[] SET ST=-ss %5
+@IF NOT [%5]==[`] SET ST=
+
+@IF NOT [%6]==[] SET TP=%6
+
+
 @IF [%1]==[-h] GOTO :HELP
-::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"-f ogg -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o -  2>nul | ffplay -i - -nodisp -vn -volume 75 -autoexit 2>nul & exit
-::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
-::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - |ffplay -i - -nodisp -vn -volume %VL% -autoexit  & exit
 
-yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
-::yt-dlp -f %YT% --cookies-from-browser chrome:"Profile 2" --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - |ffplay -i - -nodisp -vn -volume %VL% -autoexit  & exit
+@yt-dlp %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%FA%%ST% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
+
+::-af "atempo=2"
+::? -reverb_depth=10
 
 @GOTO :EOF
 :HELP
-echo ya YKy4riaOBMk best ogg
-echo ya YKy4riaOBMk best q 2:00
-echo ya YKy4riaOBMk
+echo ya YoutubeId   Volume Format ffplayFormat beginTime aTemp
+echo ` = default parametr
+echo ya YKy4riaOBMk ` bestaudio
+echo ya YKy4riaOBMk 32 251 ` 2:00
+echo ya YKy4riaOBMk 42 251 ogg
+echo ya YKy4riaOBMk ` ` ogg
+
+
+::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"-f ogg -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o -  2>nul | ffplay -i - -nodisp -vn -volume 75 -autoexit 2>nul & exit
+::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
+::yt-dlp -f %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - |ffplay -i - -nodisp -vn -volume %VL% -autoexit  & exit
+::yt-dlp --downloader ffmpeg --external-downloader-args ffmpeg:"-f %YT% %ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
+::yt-dlp -f %YT% --cookies-from-browser chrome:"Profile 2" --downloader ffmpeg --external-downloader-args ffmpeg:"%ST% %FT% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - |ffplay -i - -nodisp -vn -volume %VL% -autoexit  & exit
+
+
+
+::@yt-dlp %YT% --downloader ffmpeg --external-downloader-args ffmpeg:"%FA%%ST% -rtbufsize 15M -thread_queue_size 8M" https://www.youtube.com/watch?v=%YL% -o - 2>nul |ffplay -af "atempo=1.1" -i - -nodisp -vn -volume %VL% -autoexit 2>nul & exit
